e1940073c0664d26166d43e1237df729a222a304
GabiOC <oconnor.gabriele@gmail.com>
2015-10-12 17:18:17 -0400

    all done

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,32 @@
 # Enter your procedural solution here!
+
+def largest_prime_factor(n)
+  # efficient way using Prime class
+  Prime.prime_division(n).flatten.sort.last
+end
+
+## MORE INEFFICIENT, MANUAL WAY BELOW
+# def largest_prime_factor(num)
+#   i = 2
+#   prime_factors = []
+#
+#   while i <= num
+#     if is_prime?(i) && num % i == 0
+#       prime_factors << i
+#     end
+#     i += 1
+#   end
+#
+#   prime_factors.last
+# end
+#
+# def is_prime?(n)
+#   if n == 2
+#     true
+#   elsif n <= 1 || n % 2 == 0
+#     false
+#   else
+#     nums_to_sqrt = [*2..Math.sqrt(n)]
+#     !nums_to_sqrt.any? {|x| n % x == 0}
+#   end
+# end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,49 @@
 # Enter your object-oriented solution here!
+
+class LargestPrimeFactor
+
+  def initialize(num)
+    @num = num
+  end
+
+  def number
+    # efficient way using Prime class
+    Prime.prime_division(@num).flatten.sort.last
+  end
+end
+
+
+## MORE INEFFICIENT, MANUAL WAY BELOW
+# Enter your object-oriented solution here!
+#
+# class LargestPrimeFactor
+#
+#   def initialize(num)
+#     @num = num
+#   end
+#
+#   def number
+#     i = 2
+#     prime_factors = []
+#
+#     while i <= @num
+#       if is_prime?(i) && @num % i == 0
+#         prime_factors << i
+#       end
+#       i += 1
+#     end
+#
+#     prime_factors.last
+#   end
+#
+#   def is_prime?(n)
+#     if n == 2
+#       true
+#     elsif n <= 1 || n % 2 == 0
+#       false
+#     else
+#       nums_to_sqrt = [*2..Math.sqrt(n)]
+#       !nums_to_sqrt.any? {|x| n % x == 0}
+#     end
+#   end
+# end
